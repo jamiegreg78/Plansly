938e4529f38b1986c7ffba9de1764eca8e3f865a
Jamie <jag73@aber.ac.uk>
2023-05-04 20:49:48 +0100

    added minor comments and fixed a minor css bug I noticed

--- src/backend/Authentication.ts
+++ src/backend/Authentication.ts
@@ -1,3 +1,5 @@
+// This file contains all the functions that are used to authenticate the user, and get their information
+
 import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_KEY)
